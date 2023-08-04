fa1385533ed0ccacf3b69255e78444868373a291
mustafademiray <mustafademiray@protonmail.com>
2023-08-04 03:53:07 +0300

    multi-threaded vers

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,18 +1,21 @@
-use std::fs;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::io::prelude::*;
+use std::{
+    fs,
+    io::prelude::*,
+    net::{TcpListener, TcpStream},
+};
+use server_rs::ThreadPool;
 
 fn main() {
-    
-    let listener = 
-        TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
-    }
 
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
 }
 
 
